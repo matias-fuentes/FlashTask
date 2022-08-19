ab0af915ceb6c18634e98cb40db9b6b43f5d91b2
Matías Fuentes <mfuentesdeveloper@gmail.com>
2022-08-19 12:18:57 -0300

    FlashTask launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface todoTypes {
+    id: number;
+    title: string;
 }
+
+const App: React.FC = () => {
+    const todoTitleRef = useRef<HTMLInputElement>(null);
+    const [todos, setTodos] = useState<todoTypes[]>([]);
+
+    const todoSubmitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const todoTitle = todoTitleRef.current!.value;
+        if (todoTitle.length > 0) {
+            const todo = {
+                id: Math.random(),
+                title: todoTitle,
+            };
+            setTodos(prevTodos => [...prevTodos, todo]);
+        }
+    };
+
+    const todoDeleteHandler = (todoId: number) => {
+        setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+    };
+
+    return (
+        <Fragment>
+            <form onSubmit={todoSubmitHandler}>
+                <div>
+                    <label htmlFor='todo-title'>Todo Title</label>
+                    <input type='text' id='todo-title' ref={todoTitleRef} />
+                    <button type='submit'>ADD TODO</button>
+                </div>
+            </form>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <h2>{todo.title}</h2>
+                        <button
+                            onClick={() => {
+                                todoDeleteHandler(todo.id);
+                            }}
+                        >
+                            DELETE
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
+    );
+};
 
 export default App;
